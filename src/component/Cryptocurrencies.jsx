fcb6eb7f9ef2ba718b07f44c792abfba120e97d6
rampal <rampalyadav23082001@gmail.com>
2024-02-04 14:16:34 +0530

    second commit

--- src/component/Cryptocurrencies.jsx
+++ src/component/Cryptocurrencies.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
-
+import React, { useState,useEffect } from 'react'
+import millify from 'millify'
+import { Link } from 'react-router-dom'
+import { Card,Row,Col,Input } from 'antd'
+import { useGetCryptosQuery } from '../services/Cryptoapi'
 const Cryptocurrencies = () => {
+    // const count = simplified ? 10 : 100;
+    const {data:cryptosList,isFetching}=useGetCryptosQuery();
+    const [cryptos,setCryptos]=useState(cryptosList?.data?.coins);
+    useEffect(() => {
+        setCryptos(cryptosList?.data?.coins);
+    
+        
+      }, [cryptosList]);
+    console.log(cryptos);
   return (
-    <div>Cryptocurrencies</div>
+   <>
+   <Row gutter={[32,32]} className="crypto-card-cointainer">
+    {
+        cryptos?.map((currency)=>(
+            <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}
+            >
+                <Link to={`/crypto/${currency.id}`}>
+                < Card title={`${currency.rank}.${currency.name}`} 
+                extra={<img className='crypto-image' src={currency.iconUrl} />}
+                hoverable
+                
+                
+                > 
+               <p>Price:{millify(currency.price)}</p>
+               <p>Market Cap:{millify(currency.marketCap)}</p>
+               <p>Daily Change:{millify(currency.change)}</p>
+              
+                </Card>
+                
+                </Link>
+            </Col>
+        ))
+    }
+   </Row>
+   
+   
+   </>
   )
 }
 
